b6f390a8c18e7ed1a3963839e3a57a3b8835568c
Shmuli Bloom <shmulibloom@gmail.com>
2023-08-24 16:40:00 +1000

    improved comments for bnb_tree.py

--- bnb_tree.py
+++ bnb_tree.py
@@ -31,14 +31,12 @@ class BnB_Tree:
         self.explored_subproblems += 1
 
         # Find initial feasible solution (using a heuristic solve)
-        # UNCOMMENT
+        # UNCOMMENT when you have implemented the heuristic solver
         # self.best_cost, self.best_solution = self.root_node.get_feasible_sol()
 
         # best cost of -1 is returned when there are no feasible solutions to CGLSP
         if self.best_cost == -1:
             return self.best_cost, self.best_solution
-
-        # SHOULD WE PERFORM A COST REDUCTION LOWER BOUNDING ON ROOT NODE???
 
         # Find an initial lower bound on CGLSP using a cost reduction method
         self.root_node.get_cost_reduction_lower_bound()
@@ -54,7 +52,7 @@ class BnB_Tree:
         # the MAP relaxation of the CGLSP:
         self.root_node.solve_MAP()
 
-        # also, check if MAP relaxation was solved without any possible integer
+        # check if MAP relaxation was solved without any possible integer
         # overflow issues
         if self.root_node.MAP_solve_status == "POSSIBLE_OVERFLOW":
             raise PossibleOverflowException(
@@ -84,7 +82,7 @@ class BnB_Tree:
             return self.best_cost, self.best_solution
 
         # 2. otherwise if the MAP optimal solution is not CGLSP feasible,
-        #    then the best solution may yet not have been found so we need
+        #    then the best solution may yet not have been found, so we need
         #    to explore subproblems by branching (uing the MAP solution to
         #    branch)
         else:
@@ -95,15 +93,22 @@ class BnB_Tree:
         # than the best solution obtained so far, explore those unpruned subproblems
         while not self.unpruned_nodes.empty():
 
-            cur_node = self.unpruned_nodes.get()[1]
             # if a subproblem is in the queue that's because it needs to be explored
             # Its lower bound is lower than the currently best known solution
             # and its MAP solution (relaxation) is not feasible for CGLSP because it
             # consists of subtours
             # We need to branch the current subproblem
             # - i.e. create children subproblems and then iterate over the children
-            # subproblem requires branching
+            # subproblems
+
+            # obtain the current unpruned subproblem with the lowest lower bound
+            # by popping the min item from the unpruned nodes priority queue
+            cur_node = self.unpruned_nodes.get()[1]
+
             self.branched_subproblems += 1
+
+            # if we have branched a multiple of update_frequency times
+            # output solver progress updates to the console
 
             if self.branched_subproblems % self.update_frequency == 0:
                 dashed_line = "-" * 80
@@ -121,8 +126,10 @@ class BnB_Tree:
                 print("Current subproblems still required to process: ",
                       self.unpruned_nodes.qsize())
 
+            # branch the current subproblem just popped of the queue
             children_nodes = self.branch(cur_node)
 
+            # for each of the branched subproblems, process them
             for node in children_nodes:
 
                 self.explored_subproblems += 1
@@ -146,24 +153,19 @@ class BnB_Tree:
                 # problem for this subproblem.
                 # The obtained optimal MAP solution for this subproblem could result in:
                 #   1. Pruning the subproblem, if the MAP optimal cost which is a lower
-                #      bound on the subproblem is at least as high as the best cost
+                #      bound on the subproblem, is at least as high as the best cost
                 #      known so far, as then this subtree can't contain the optimal
-                #      solution, OR
+                #      solution, or
                 #   2. Finding a new best solution if the MAP optimal solution is CGLSP
                 #      feasible and has lower cost then our current best cost,
-                #      as well as Pruning the subproblem, because then the MAP optimal
+                #      as well as pruning the subproblem, because then the MAP optimal
                 #      solution is the optimal solution for this subproblem and so we
-                #      don't need to explore the subproblem further, OR
+                #      don't need to explore the subproblem further, or
                 #   3. The need to branch this subproblem further if we can't
                 #      establish a higher lower bound on the subproblem than
                 #      our current best solution
-                # try:
 
                 node.solve_MAP()
-
-                # except TimeoutError:
-                #     print("Google AP Solver took more than 10 seconds to solve \
-                #           MAP for subproblem")
 
                 # Check if there is no feasible solution for the MAP relaxation of the
                 # subproblem. If this is the case then there is no feasible solutions
@@ -193,12 +195,11 @@ class BnB_Tree:
                 # then if:
                 #        1. the MAP found a feasible solution to the CGLSP, then this
                 #           solution is the best known solution so far
-                #           and of course this solution is the best for this subproblem,
+                #           and this solution is the best for this subproblem,
                 #           so we don't need to explore it further
                 #        2. otherwise, the best solution may be in this subset of the
                 #           solution space, in which case we need to use the MAP
                 #           solution to branch this subproblem
-
                 else:
                     if node.MAP_sol_CGLSP_feasible():
                         self.optimal_subproblem_solutions_found += 1
@@ -211,9 +212,12 @@ class BnB_Tree:
                         self.prune_queue()
 
                     else:
+                        # if the MAP solution wasn't feasible then we need to
+                        # branch this subproblem
                         node_priority = node.lower_bound
                         self.unpruned_nodes.put((node_priority, node))
 
+        # Output fianl solution statistics once the optimal solution has been found
         dashed_line = "-" * 80
         print(dashed_line)
         print(f"Solution statistics at end of solve: \n")
@@ -232,7 +236,7 @@ class BnB_Tree:
 
     def branch(self, node):
         # takes a subproblem that may contain the optimal solution to CGLSP
-        # and branche the subproblem based on the subtourof its MAP optimal solution
+        # and branches the subproblem based on the subtour of its MAP optimal solution
         # with the least edges in common with the included edges of the subproblem
 
         # get the list of non included edges of minimal subtour
@@ -246,11 +250,11 @@ class BnB_Tree:
         for i in range(num_children_nodes):
 
             # determine the edges to fix in each of the branched subproblems, child
-            # subproblem j= = j=1,...len(minimal_subtour_non_included_edges)
+            # subproblem, j=1...len(minimal_subtour_non_included_edges)
             # for child subproblem j,
-            # exclude edge j of the minimal_subtour non included edges
+            # exclude edge j of the minimal_subtour_non_included_edges
             # and the current subproblem excluded edges, and
-            # include edges 1...j-1 of the minimal subtour non included edges
+            # include edges 1...j-1 of the minimal_subtour_non_included_edges
             # and the current subproblem included edges
 
             new_included_edges = minimal_subtour_non_included_edges[0:i]
@@ -258,6 +262,7 @@ class BnB_Tree:
             child_included_edges = new_included_edges + node.included_edges
             child_excluded_edges = new_excluded_edge + node.excluded_edges
 
+            # create the new child subproblem
             child_node = Node(
                 cost_matrix=self.root_cost_matrix,
                 instance_size=self.instance_size,
@@ -265,11 +270,13 @@ class BnB_Tree:
                 excluded_edges=child_excluded_edges,
                 lower_bound=node.lower_bound,
             )
+            # append it to the list of child subproblems to be process by the branch
+            # and bound algorithm next
             children_nodes.append(child_node)
 
-        # return children nodes in the order of most number of new included edge
+        # return the children nodes in the order of most number of new included edge
         # restrictions
-        # we will process them in this order as the more constrained the subproblems
+        # we will process them in this order, as the more constrained the subproblems
         # are, the more likely they are to contain optimal solutions without as
         # much additional branching
         # which will allow us to prune the branch and bound tree earlier
@@ -278,20 +285,22 @@ class BnB_Tree:
         return children_nodes[::-1]
 
     def minimal_subtour_edges(self, node_included_edges, node_MAP_sol):
-        # for each subtour in MAP sol find how many non included edges it has
-        # return edges of subtour with minimum number of edges not in in nodes included
+        # for each subtour in MAP solution, find how many non included edges it has
+        # return edges of subtour with minimum number of edges not in the nodes included
         # edges
-        # ideally order the edges according to carapento criteria, but for now we will
-        # just return them in order of the tour as per simplified L version
+        # ideally order the edges according to Carpaneto criteria, but for now we will
+        # just return them in order of the tour as per simplified Laporte version
 
+        # obtain all subtours of the MAP solution
         subtours = self.find_subtours(node_MAP_sol)
 
         # the subtour with the minimum number of non included edges must have less than
         # a full tour of non included edges
         # as the subtour itself already has less than a full tour of edges
+        # hence we set this as the upper bound for minimizing over the subtours
         min_non_included_edges = self.instance_size
 
-        # iterated through all the subtours and find the subtour with the minimum numbe
+        # iterated through all the subtours and find the subtour with the minimum number
         # of edges not already included in the current subproblem
         for subtour in subtours:
             subtour_non_included_edges = set(
@@ -308,7 +317,10 @@ class BnB_Tree:
         return minimal_subtour_non_included_edges
 
     def find_subtours(self, node_MAP_sol):
+        # find all subtours of the MAP optimal solution
 
+        # create a copy of the MAP solution as the solution is used by other
+        # methods and passed by reference
         sol = node_MAP_sol.copy()
 
         # create mapping between consecutive pairs of vertices
@@ -317,24 +329,25 @@ class BnB_Tree:
         for out_vertex, in_vertex in node_MAP_sol:
             consecutive_vertex_mapping[out_vertex] = in_vertex
 
+        # get all teh subtours
         subtours = []
         # while there are still subtours
         while sol:
             # get the next subtour
             subtour = []
-            # start the next subtour from the first remaining vertex in
-            # MAP sol
+            # start the next subtour from the first remaining vertex in the
+            # MAP solution
             out_vertex, _ = sol[0]
             # initialize a list of vertices already visited in this subtour
             visited_vertices = {out_vertex}
             while True:
-                # find the next vertex in subtour
+                # find the next vertex in the subtour
                 in_vertex = consecutive_vertex_mapping[out_vertex]
-                # add next edge in subtour to the subtour
+                # add next edge in  the subtour to the subtour
                 subtour.append((out_vertex, in_vertex))
                 # remove the edge from the list of edges in the solution
                 sol.remove((out_vertex, in_vertex))
-                # if we have already visted this in vertex then we have completed
+                # if we have already visted this in_vertex then we have completed
                 # this subtour
                 if in_vertex in visited_vertices:
                     subtours.append(subtour)
@@ -348,11 +361,20 @@ class BnB_Tree:
         return subtours
 
     def prune_queue(self):
-        # remove all subproblems from queue whose lower bound is no better than
-        # the best cost found so far
+        # remove all subproblems from the unpruned subproblems queue
+        # whose lower bound is no better than the best cost found so far
 
+        # create a new queue to which we will add all subproblems that can't yet
+        # be pruned
         new_queue = queue.PriorityQueue()
 
+        # iterate through the queueand insert all subproblems that can't yet be
+        # pruned in the new subproblems queue new_queue. Since the queue is in non
+        # decreasing order of lower bounds, there first x subproblems in the queue
+        # won't yet be pruneable.
+        #  Once you reach the point in the queue where all remaining subproblems have
+        # lower bound higher higher than the best determined cost so far, we can
+        # just delete the original queue
         while not self.unpruned_nodes.empty():
             lower_bound, node = self.unpruned_nodes.get()
 
